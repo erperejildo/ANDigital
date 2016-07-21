c89116a26a57b1ce3aa8ef93364129678cab183b
Daniel <d15_1_89@msn.com>
2016-07-21 14:44:27 +0100

    Input changes search

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,17 +9,32 @@
  */
 angular.module('andigitalApp')
   .controller('MainCtrl', function ($scope, apiFactory) {
-    
+
   	function getRecommended() {
-        apiFactory.getRecommended()
+        apiFactory.getRecommended($scope.text)
         .then(function (response) {
             $scope.venues = response.data.response.venues;
             console.log($scope.venues);
         }, function (error) {
             console.log(error);
         });
-    }
+    };
 
-    getRecommended();
+    $scope.search = function() {
+    	getRecommended();
+    };
+
+  	// detect platform for menus
+  	$scope.getMenu = function(url) {
+  		if (url) {
+	  		if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+	  			// mobile
+	  			return url.mobileUrl;
+	  		} else {
+	  			// desktop
+	  			return url.url;
+	  		}
+	  	}
+  	};
 
   });
