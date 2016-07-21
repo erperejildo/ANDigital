c5dfb72f88b8fc6ae27360502714e0d790b60cd7
Daniel <d15_1_89@msn.com>
2016-07-21 20:01:08 +0100

    Last commit

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,25 +16,50 @@ angular.module('andigitalApp')
         apiFactory.getRecommended($scope.text)
         .then(function (response) {
             $scope.venues = response.data.response.venues;
-            console.log($scope.venues);
-            // no results
+
             if ($scope.venues.length == 0) {
             	$scope.messageText = 'No results';
             	$scope.message = false;
+            	$scope.showOrder = false;
+            } else {
+            	$scope.showOrder = true;
             }
 
             $scope.loading = false;
         }, function (error) {
+        	$scope.showOrder = true;
             $scope.loading = false;
             $scope.error   = true;
         });
     };
+
+    // MOCKUP
+    /*$scope.text = 'sushi';
+    getRecommended();*/
 
     $scope.search = function() {
     	$scope.message  = true;
     	$scope.loading  = true;
     	$scope.error    = false;
     	getRecommended();
+    };
+
+    // change venues order
+    $scope.orderBy = function(order) {
+
+    	if ($scope.order != order) {
+    		$scope.order = order;
+    		$scope.orderVenue = '-stats.' + order;
+			$scope.up = false;
+    	} else {
+    		if ($scope.up) {
+		    	$scope.orderVenue = '-stats.' + order;
+    			$scope.up = false;
+    		} else {
+		    	$scope.orderVenue = 'stats.' + order;
+    			$scope.up = true;
+    		}
+    	}
     };
 
   	// detect platform for menus
--- app/scripts/directives/venue.js
+++ app/scripts/directives/venue.js
@@ -9,17 +9,7 @@
  angular.module('andigitalApp')
  .directive('venue', function () {
  	return {
- 		template: ['<div id="venues">',
- 		'<div class="venue" ng-repeat="venue in venues"><div class="box">',
-	 		'<div class="name"><a href="{{venue.url}}" target="_blank">{{venue.name}}</a></div>',
-	 		'<div class="address">{{venue.location.formattedAddress[0]}}, {{venue.location.formattedAddress[1]}}</div>',
-	 		'<a href="{{getMenu(venue.menu)}}" target="_blank" class="link" ng-if="venue.hasMenu">Menu</a>',
-	 		'<a href="{{venue.delivery.url}}" target="_blank" class="link" ng-if="venue.delivery">Order</a>',
-	 		'<span class="phone">{{venue.contact.formattedPhone}}</span>',
-	 		'<a href="https://twitter.com/{{venue.contact.twitter}}" target=="_blank" class="twitter" ng-if="venue.contact.twitter"><img src="images/twitter.svg" /></a>',
-	 		'</div>',
- 		'</div></div>',
- 		'</div>'].join(''),
+		templateUrl: 'views/directives/venue.html',
  		restrict: 'E',
  		link: function (scope, element, attrs) {
  		}
