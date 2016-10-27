5e6e75f32d0ac4ddacf8d24a0db8bed109fa936c
Daniel <info@rodriguezdaniel.com>
2016-10-27 14:04:09 +0000

    Update apifactory.js

--- app/scripts/services/apifactory.js
+++ app/scripts/services/apifactory.js
@@ -15,7 +15,7 @@ angular.module('andigitalApp')
     // API config
     var cliendID     = 'ATYTFKNQMUBELBOYI0XRL3VMSRY2EFR2L44RCLN3YEPE2V0F';
     var clientSecret = '3PPYKXTGHOE2YXFFAOIAZQOIC4RDZZSAMAZIXV2YAHWX5HJD';
-    var urlBase  	 = 'https://api.foursquare.com/v2/venues/search?client_id=' + cliendID + '&client_secret=' + clientSecret + '&v=20130815&ll=40.7,-74&query=';
+    var urlBase  	   = 'https://api.foursquare.com/v2/venues/search?client_id=' + cliendID + '&client_secret=' + clientSecret + '&v=20130815&ll=40.7,-74&query=';
 
     apiFactory.getRecommended = function (text) {
         return $http.get(urlBase + text);
